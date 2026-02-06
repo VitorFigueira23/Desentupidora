aee3346790a825c907e78449e6c8115795f77ea8
Vitor <vitor.figueira@chan3.com.br>
2026-02-06 02:26:58 -0300

    build

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
   envDir: path.resolve(import.meta.dirname),
   root: path.resolve(import.meta.dirname, "client"),
   build: {
-  outDir: "dist", // Sem /public
+  outDir: "build", // Sem /public
   emptyOutDir: true,
   },
   server: {
